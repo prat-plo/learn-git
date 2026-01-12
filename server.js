4bcc48d3fcfa2dd32e788b2b1720d7034b92d0df
pratchaya <prat556fr@gmail.com>
2026-01-13 03:17:45 +0700

    add login

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 const express = require("express");
-const app = express();
+const bodyParser = require('body-parser')
+
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Server is running on port http://localhost:${port}`));
+const app = express();
+
+app.use(bodyParser.json());
+
+const mockUsersData = [
+    {
+        username: "admin",
+        password: "admin"
+    },
+    {
+        username: "guest",
+        password: "guest"
+    }];
+
+//login
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    const user = mockUsersData.find((u) => u.username === username && u.password === password);
+    if (user) {
+        res.status(200).json({ message: "Login successful" });
+    } else {
+        res.status(401).json({ message: "Invalid username or password" });
+    }
+});
+
+app.listen(port, () =>
+  console.log(`Server is running on port http://localhost:${port}`));
